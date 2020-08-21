d24e8b79111401224d84460337bdec4335710891
Federico Degiovanni <federicoadegiovanni@gmail.com>
2020-08-21 01:30:19 -0300

    Segundo flujo

--- detail.php
+++ detail.php
@@ -5,7 +5,7 @@ require __DIR__ .  '/vendor/autoload.php';
 $link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 
                 "https" : "http") . "://" . $_SERVER['HTTP_HOST'] .  
                 $_SERVER['REQUEST_URI']; 
-$link = "http://localhost:3000";
+//$link = "http://localhost:3000";
 
 
 // Agrega credenciales
