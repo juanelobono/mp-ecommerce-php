d979d5ec968565575f39105703616bd2e1c9ac08
Federico Degiovanni <federicoadegiovanni@gmail.com>
2020-08-22 00:18:56 -0300

    Notificacion

--- detail.php
+++ detail.php
@@ -2,9 +2,7 @@
 
 <?php
 require __DIR__ .  '/vendor/autoload.php';
-$link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 
-                "https" : "http") . "://" . $_SERVER['HTTP_HOST'] .  
-                $_SERVER['REQUEST_URI']; 
+$link = "https://fdegiovanni-mp-commerce-php.herokuapp.com";
 //$link = "http://localhost:3000";
 
 
